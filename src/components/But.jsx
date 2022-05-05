565170b01a2cd59968c11b6bc312035985450098
hamidsouhir <hamidsouhir475@gmail.com>
2022-05-05 18:54:31 +0200

    first work shop

---
+++ src/components/But.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import { Button, ButtonGroup } from 'react-bootstrap'
+
+const But = () => {
+  return (
+    <div><ButtonGroup aria-label="Basic example">
+    <Button variant="secondary">Left</Button>
+    <Button variant="secondary">Middle</Button>
+    <Button variant="secondary">Right</Button>
+  </ButtonGroup></div>
+  )
+}
+
+export default But
